09cb852c063a51a7d6767a3f63c593d48a898b1e
dalythu <dalythu@gmail.com>
2026-03-09 14:40:32 -0400

    created room maodel and light schema

---
+++ models/Room.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const lightSchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    on: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { _id: false }
+);
+
+const roomSchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    occupied: {
+      type: Boolean,
+      default: false,
+    },
+    lights: {
+      type: [lightSchema],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Room = mongoose.model("Room", roomSchema);
+
+export default Room;
